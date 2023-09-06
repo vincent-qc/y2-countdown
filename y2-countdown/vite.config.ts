36c87e4982da5bebe6bc03442956743f1fec78c0
Vincent Qi <vincent.qi@hotmail.com>
2023-09-06 00:04:47 -0700

    fix

--- y2-countdown/vite.config.ts
+++ y2-countdown/vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/y2-countdown/" : "/",
   plugins: [react()],
 });
